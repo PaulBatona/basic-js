823573135ddf1c9b3f47f4172213855a1b2b9a7d
PaulBatona <60320870+PaulBatona@users.noreply.github.com>
2020-02-17 10:03:16 +0300

    feat: update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,10 +1,10 @@
 module.exports = function countCats(backyard) {
-  let sum = 0;
+  let number = 0;
   for(var x = 0; x < arr.length; x++){
     for(var j = 0; j < arr[x].length; j++) {
       if (arr[x][j]=="^^"){
-        sum++;
+        number++;
       };
     };
-  return sum;
+  return number;
 };
