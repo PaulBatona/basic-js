5ff279c085799d4b1706b62c5633578ed83a127a
PaulBatona <60320870+PaulBatona@users.noreply.github.com>
2020-02-24 19:08:55 +0300

    feat: update task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,41 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(arr.length===0){
+        return [];
+    }
+    else if(arr.length==1){
+        if(arr[0]==='--discard-prev' || arr[0]==='--discard-next' || arr[0]==='--double-next' || arr[0]==='--double-prev'){
+            return [];
+        }
+    }
+    else if(Array.isArray(arr)){
+        for(let i=0; i<arr.length; i++){
+            if(arr[i]=='--discard-next'){
+                if(i==arr.length-1){continue}
+                else{arr.splice(i,2);}
+            }
+            else if(arr[i]=='--discard-prev'){
+                if(i==0){continue}
+                else{arr.splice(i-1,2);}
+            }
+            else if(arr[i]=='--double-next'){
+                if(i==arr.length-1){continue}
+                else{
+                    arr.splice(i,1);
+                    arr.splice(i,0,arr[i]);
+                }
+            }
+            else if(arr[i]=='--double-prev'){
+                if(i==0){continue}
+                else{
+                    arr.splice(i,1);
+                    arr.splice(i-1,0,arr[i-1]);
+                }
+            }
+        }
+
+        return(arr);
+    }
+    else{
+        throw Error;
+    }
 };
