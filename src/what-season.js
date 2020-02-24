c4acbe6498b54c8096138debbae9ad89de83b4bf
PaulBatona <60320870+PaulBatona@users.noreply.github.com>
2020-02-24 15:44:34 +0300

    feat: update task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,22 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    let month = date.getMonth();
+    if(date === undefined){
+      return 'Unable to determine the time of year!';
+    }
+    else{
+      if(month==11 || month==0 || month==1){
+      return 'winter';
+    }
+    else if(month==2 || month==3 || month==4){
+      return 'spring';
+    }
+    else if(month==5 || month==6 || month==7){
+      return 'summer';
+    }
+    else if (month==8 || month==9 || month==10){
+      return 'autumn';
+    }
+  }
+    
+
 };
