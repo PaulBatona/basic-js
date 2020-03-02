df2e7ac45fc74b5f1bc6aca8543146b291ea0e34
PaulBatona <60320870+PaulBatona@users.noreply.github.com>
2020-03-02 17:46:57 +0300

    feat: update task

--- src/transform-array.js
+++ src/transform-array.js
@@ -10,22 +10,22 @@ module.exports = function transform(arr) {
     else if(Array.isArray(arr)){
         for(let i=0; i<arr.length; i++){
             if(arr[i]=='--discard-next'){
-                if(i==arr.length-1){continue}
+                if(i==arr.length-1){arr.splice(i,1);}
                 else{arr.splice(i,2);}
             }
             else if(arr[i]=='--discard-prev'){
-                if(i==0){continue}
+                if(i==0){arr.splice(i,1);}
                 else{arr.splice(i-1,2);}
             }
             else if(arr[i]=='--double-next'){
-                if(i==arr.length-1){continue}
+                if(i==arr.length-1){arr.splice(i,1);}
                 else{
                     arr.splice(i,1);
                     arr.splice(i,0,arr[i]);
                 }
             }
             else if(arr[i]=='--double-prev'){
-                if(i==0){continue}
+                if(i==0){arr.splice(i,1);}
                 else{
                     arr.splice(i,1);
                     arr.splice(i-1,0,arr[i-1]);
