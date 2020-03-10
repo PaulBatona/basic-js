a338edd5c81e7a20a338f65050256abb8751c6a9
PaulBatona <60320870+PaulBatona@users.noreply.github.com>
2020-03-10 20:37:02 +0300

    feat: complete task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,12 +1,13 @@
 module.exports = class DepthCalculator {
     calculateDepth(arr, depth) {
-        //console.log(JSON.stringify(arr));
-        if(!depth){depth=1;}
+        var a;
+        if(depth==undefined){depth=1;}
+        a=depth;
         for(let i=0; i<arr.length; i++){
             if(Array.isArray(arr[i])){
-                depth = Math.max(this.calculateDepth(arr[i],depth+1),depth);
+                a = Math.max(this.calculateDepth(arr[i],depth+1),a);
             }
         }
-        return depth;
+        return a;
     }
 };
