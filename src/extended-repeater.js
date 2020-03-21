659adc52844571ee07b6e79ac289190605d5e623
PaulBatona <60320870+PaulBatona@users.noreply.github.com>
2020-03-21 21:09:38 +0300

    feat: complete extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,30 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    str = String(str);
+    if(options.repeatTimes === undefined) options.repeatTimes=1;
+    if(options.separator === undefined) options.separator='+';
+    if(options.addition === undefined) {
+        options.addition='';
+    }
+    else{
+        options.addition = String(options.addition)
+    }
+    if(options.additionRepeatTimes === undefined) options.additionRepeatTimes=1;
+    if(options.additionSeparator === undefined) options.additionSeparator='|';
+
+    let res = '';
+    for(let i=0; i<options.repeatTimes; i++){
+        res = res + str;
+        if(options.addition!=''){
+            for(let j=0; j<options.additionRepeatTimes; j++){
+                res = res + options.addition;
+                if(j!==options.additionRepeatTimes-1){
+                    res = res + options.additionSeparator;
+                }
+            }
+        }
+        if(i!==options.repeatTimes-1){
+            res = res+options.separator;
+        }
+    }
+    return res;
 };
-  
